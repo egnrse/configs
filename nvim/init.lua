75b501bba780c01e3d2d18eeb0febeda2242b8d7
egnrse <30458827+egnrse@users.noreply.github.com>
2024-11-09 21:01:52 +0100

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,13 +2,19 @@ vim.cmd('source ~/.vimrc')		-- import some settings from vim
 
 -- General Settings
 vim.o.number = true				-- line numbers
-vim.o.relativenumber = true		-- Enable relative line numbers
-vim.o.tabstop = 4				-- Number of spaces a tab represents
-vim.o.shiftwidth = 4			-- Number of spaces for each indentation
-vim.o.smartindent = true		-- Automatically indent new lines
-vim.o.wrap = false				-- Disable line wrapping
-vim.o.cursorline = true			-- Highlight the current line
-vim.o.termguicolors = true		-- Enable 24-bit RGB colors
+vim.o.relativenumber = true		-- enable relative line numbers
+vim.o.tabstop = 4				-- number of spaces a tab represents
+vim.o.shiftwidth = 4			-- number of spaces for each indentation
+vim.o.mouse = a					-- allow all mouse interations
+
+vim.o.hls = true				-- search hilghlighting [noh]
+vim.o.smartcase = true			-- make search case insensitive for only lowercase search
+vim.o.smartindent = true		-- automatically indent new lines
+vim.o.wrap = true				-- line wrapping
+
+vim.o.visualbell = true
+vim.o.cursorline = true			-- highlight the current line
+vim.o.termguicolors = true		-- enable 24-bit RGB colors
 
 vim.api.nvim_exec ('language en_US', true)
 
