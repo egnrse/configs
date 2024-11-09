f58e6431ba117bc2f0eb1aacd9deb97f458e4414
egnrse <30458827+egnrse@users.noreply.github.com>
2024-11-09 20:57:26 +0100

    Update maps.lua
    buffer, split, terminal updates

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -1,15 +1,8 @@
 -- Keymappings
 --
 -- TODO: <leader>
--- bd: close buffer
--- relative Number
--- ft: terminal
--- | or -: split window
--- wm: maximize
--- <tab> to swich/close buffer
--- [b next buf.
--- ]b prev buf.
---
+-- relative Number toggle
+
 vim.g.mapleader = ' '		-- Space as the leader key
 
 -- (mode, KeyMap, function [, options])
@@ -18,19 +11,39 @@ local function map(mode, lhs, rhs)
 end
 
 -- general
-map("n", "<leader>w", "<CMD>update<CR>")	--save
-map("v", "<C-c>", '"+y')					--copy (v)
-map("n", "<C-c>", '"+yy')					--copy line
-map("n", "<C-v>", "<C-q>")
+map("n", "<leader>w", "<CMD>update<CR>")	--save (n)
+map("v", "<C-c>", '"+y')					--copy clipboard (v)
+map("n", "<C-c>", '"+yy')					--copy line (n)
+map("i", "<C-v>", '<C-r>+')					--inset clipboard (i)
+map("n", "<C-v>", "<C-q>")					--visual block mode (n)
 
 --vim.api.nvim_del_keymap('n', '<C-v>')		-- delete a mapping
 
--- FileManager
-map("n", "<leader>e", "<CMD>NvimTreeToggle<CR>")
-map("n", "<leader>r", "<CMD>NvimTreeFocus<CR>")
-vim.cmd([[autocmd FileType NvimTree nmap <buffer> <space> <CR>]])	-- in nvim-tree map space to enter ('<CR>')
 
--- Windows
+-- WINDOWS --
+-- Buffers
+map("n", "<leader><Tab>", "<CMD>bnext<CR>") --next (n)
+map("n", "<leader>bn", "<CMD>bnext<CR>")	--next (n)
+map("n", "<leader><S-Tab>", "<CMD>bp<CR>")	--previous (n)
+map("n", "<leader>bp", "<CMD>bprevious<CR>")--previous (n)
+map("n", "<leader>q", "<CMD>bd<CR>")		--close (n)
+map("n", "<leader>bd", "<CMD>bd<CR>")		--close (n)
+map("n", "<leader>bD", "<CMD>bd!<CR>")		--close (n)
+
+-- Managing
+map("n", "<leader>|", ":vsplit<CR>")		--vertical split
+map("n", "<leader>-", ":split<CR>")			--horizontal split
+map("n", "<leader>wm", ":only<CR>")			--maximize current
+
+--Terminal
+local term = ""
+if vim.fn.has('win64') == 1 then
+	term = "powershell.exe"
+end
+map("n", "<leader>th", ":belowright split | terminal " .. term .."<CR>")	--open terminal horizontal
+map("n", "<leader>tv", ":belowright vsplit | terminal " .. term .."<CR>")	--open terminal vertical
+map("t", "<Esc>", "<C-\\><C-n>")			-- swtch from terminal-mode to normal-mode
+
 -- Navigation
 map("n", "<C-h>", "<C-w>h")
 map("n", "<C-l>", "<C-w>l")
@@ -42,6 +55,11 @@ map("n", "<C-Left>", "<C-w><")
 map("n", "<C-Right>", "<C-w>>")
 map("n", "<C-Up>", "<C-w>+")
 map("n", "<C-Down>", "<C-w>-")
+
+-- FileManager
+map("n", "<leader>e", "<CMD>NvimTreeToggle<CR>")
+map("n", "<leader>r", "<CMD>NvimTreeFocus<CR>")
+vim.cmd([[autocmd FileType NvimTree nmap <buffer> <space> <CR>]])	-- in nvim-tree map space to enter ('<CR>')
 
 
 -- Custom Commands
