829b904f674dec41819d426b04d7e71eb52fe8f3
Elia Nitsche <nitscheelia@gmail.com>
2024-12-06 03:36:08 +0100

    updated nvim mappings

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -1,5 +1,12 @@
 -- Keymappings
 --
+-- sorted into:
+-- 	- General		(save, clipboard, ..)
+-- 	- Windows:		(Buffer management)
+-- 		Buffers (Navigation), Size Managing, Terminal, FileManager
+-- 	- Custom Commands / Other
+--
+--
 -- TODO: <leader>
 -- relative Number toggle
 -- understand fix cmp
@@ -7,12 +14,13 @@
 
 vim.g.mapleader = ' '		-- Space as the leader key
 
--- (mode, KeyMap, function [, options])
+-- function for easier/faster remapping of keys
+-- (vim-mode, KeyMap, function [, options])
 local function map(mode, lhs, rhs)
     vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = true })
 end
 
--- general
+-- General
 map("n", "<leader>w", "<CMD>update<CR>")	--save (n)
 map("v", "<C-c>", '"+y')					--copy clipboard (v)
 map("n", "<C-c>", '"+yy')					--copy line (n)
@@ -25,7 +33,7 @@ map("c", "<Tab>", "<C-z>")					--tab-autocompletation in commands (c)
 
 
 -- WINDOWS --
--- Buffers
+-- Buffer (Navigation)
 map("n", "<leader><Tab>", "<CMD>bnext<CR>") --next (n)
 map("n", "<leader>bn", "<CMD>bnext<CR>")	--next (n)
 map("n", "<leader><S-Tab>", "<CMD>bp<CR>")	--previous (n)
@@ -34,10 +42,20 @@ map("n", "<leader>q", "<CMD>bd<CR>")		--close (n)
 map("n", "<leader>bd", "<CMD>bd<CR>")		--close (n)
 map("n", "<leader>bD", "<CMD>bd!<CR>")		--close (n)
 
--- Managing
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-l>", "<C-w>l")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-j>", "<C-w>j")
+
+-- Size Managing
 map("n", "<leader>|", ":vsplit<CR>")		--vertical split
 map("n", "<leader>-", ":split<CR>")			--horizontal split
 map("n", "<leader>wm", ":only<CR>")			--maximize current
+
+map("n", "<C-Left>", "<C-w><")
+map("n", "<C-Right>", "<C-w>>")
+map("n", "<C-Up>", "<C-w>+")
+map("n", "<C-Down>", "<C-w>-")
 
 --Terminal
 local term = ""
@@ -48,25 +66,13 @@ map("n", "<leader>th", ":belowright split | terminal " .. term .."<CR>")	--open 
 map("n", "<leader>tv", ":belowright vsplit | terminal " .. term .."<CR>")	--open terminal vertical
 map("t", "<Esc><Esc>", "<C-\\><C-n>")			-- swtch from terminal-mode to normal-mode
 
--- Navigation
-map("n", "<C-h>", "<C-w>h")
-map("n", "<C-l>", "<C-w>l")
-map("n", "<C-k>", "<C-w>k")
-map("n", "<C-j>", "<C-w>j")
-
--- Resizing
-map("n", "<C-Left>", "<C-w><")
-map("n", "<C-Right>", "<C-w>>")
-map("n", "<C-Up>", "<C-w>+")
-map("n", "<C-Down>", "<C-w>-")
-
 -- FileManager
 map("n", "<leader>e", "<CMD>NvimTreeToggle<CR>")
 map("n", "<leader>r", "<CMD>NvimTreeFocus<CR>")
 vim.cmd([[autocmd FileType NvimTree nmap <buffer> <space> <CR>]])	-- in nvim-tree map space to enter ('<CR>')
+vim.api.nvim_create_user_command('E', ':NvimTreeToggle', {})
 
 
 -- Custom Commands
 vim.api.nvim_create_user_command('W', ':w', {})			--save
 vim.api.nvim_create_user_command('Q', ':qa', {})		--close
-vim.api.nvim_create_user_command('E', ':NvimTreeToggle', {})
