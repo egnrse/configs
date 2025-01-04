05ed49e09ae9384f913d2a2f976f65367d8246cd
elia@15cn1xxx <nitscheelia@gmail.com>
2025-01-04 10:37:07 +0100

    nwg-drawer: new start-menu

---
+++ nwg-drawer/restart-nwg-drawer.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+# restart nwg-drawer
+# (by egnrse)
+#
+# in the background (-r), with some custom arguments
+
+killall nwg-drawer
+sleep 0.5
+# start in the background
+#hyprctl dispatch exec -- nwg-drawer -r
+hyprctl dispatch exec -- nwg-drawer -r -c 8 -spacing 10 -wm 'hyprland'
+
