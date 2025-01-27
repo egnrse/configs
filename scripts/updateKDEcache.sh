d09e1fce666af6ad204248f86c48fdb2d171b2d5
Elia Nitsche <nitscheelia@gmail.com>
2025-01-27 20:57:24 +0100

    scripts: added updateKDEcache (fix for dolphin)

---
+++ scripts/updateKDEcache.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+# updates the KService desktop-file system configuration cache
+# (by egnrse)
+#
+# (see `man 8 kbuildsycoca6`)
+
+if ! pacman -Q archlinux-xdg-menu >/dev/null 2>&1; then
+	echo "'archlinux-xdg-menu' not found this might not work, install it or change the 'XDG_MENU_PREFIX' within this script."
+fi
+XDG_MENU_PREFIX=arch-
+kbuildsycoca6 --noincremental
