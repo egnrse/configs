4f15b96e8876288ce711ff3a9f3dad894b91d4f5
Elia Nitsche <nitscheelia@gmail.com>
2025-01-30 02:35:58 +0100

    screenshot.sh

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,6 +1,12 @@
 #!/bin/env bash
 # copy part of the screen into the system clipboard
-# Needs: grim slurp wl-copy
+# Needs: grim slurp wl-clipboard
 # (by egnrse)
 
-grim -g "$(slurp)" - | wl-copy
+arg1=$1
+
+if [ -z "${arg1}" ]; then
+	grim -g "$(slurp)" - | wl-copy
+else
+	echo "unkown argument '${arg1}', this script does not take any arguments."
+fi
