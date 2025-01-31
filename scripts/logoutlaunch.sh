329e69fe22b0994467990d68625326537e0b1714
Elia Nitsche <nitscheelia@gmail.com>
2025-01-31 21:09:18 +0100

    wlogout|logoutlaunch.sh: update/added dependency savety+looks

--- scripts/logoutlaunch.sh
+++ scripts/logoutlaunch.sh
@@ -1,18 +1,25 @@
 #!/bin/env bash
 
 # launches wlogout with custom styling, or closes it if it is already running
-# in one of two modes (selected with args $1):
+# in one of two modes (selected with arg $1):
 # 	1: 6 options
 # 	2: 4 options
 #
 # Needs:
 # 	- wlogout
-# 	- hyprctl (to get the screenresolution)
+# 	- hyprctl (from hyprland, to get the screenresolution)
+# 	- jq
 # 	- and the launched theme files (layout-*/style-*.css)
 # 	- (style-*.css needs egnrseTheme.css to work)
 #
 #	edited by egnrse (https://github.com/egnrse/configs)
-#	(maybe original?: https://github.com/sejjy/mechabar/)
+#	(maybe original?: https://github.com/prasanthrangan/hyprdots)
+
+# used for notifications
+scriptName="logoutlaunch.sh"
+
+# configuration directories
+config="$HOME/.config/wlogout"
 
 # Check if wlogout is already running, and terminate if so
 if pgrep -x "wlogout" >/dev/null; then
@@ -20,20 +27,34 @@ if pgrep -x "wlogout" >/dev/null; then
   exit 0
 fi
 
-# Define configuration directories
-config="$HOME/.config/wlogout"
+# test if package ($1) is available (notifies the user if not)
+available() {
+	package=$1
+	if ! pacman -Q "${package}" >>/dev/null 2>&1; then
+		echo "${scriptName}: package '${package}' missing"
+		notify-send "${scriptName}: package '${package}' missing" &
+		return 1
+	fi
+	return 0
+}
+available wlogout
+available hyprland
+available jq
 
-# Style selection
+
+# style selection
 [ -z "${1}" ] || style_index="${1}"
 layout="${config}/layout-${style_index}"
 style="${config}/style-${style_index}.css"
 
 # Fallback if style or layout file is not found
 if [ ! -f "${layout}" ] || [ ! -f "${style}" ]; then
-  echo "ERROR: Config ${style_index} not found..."
-  style_index=1
-  layout="${config}/layout-${style_index}"
-  style="${config}/style-${style_index}.css"
+	echo "${scriptName}: Config ${style_index} not found (in '${config}/')"
+	notify-send -a ${scriptName} "${scriptName}: Config '${style_index}' not found"\
+		"(in '${config}/')" &
+	style_index=1
+	layout="${config}/layout-${style_index}"
+	style="${config}/style-${style_index}.css"
 fi
 
 # Detect monitor resolution and scaling
@@ -43,22 +64,24 @@ scale_factor=$(hyprctl -j monitors | jq '.[] | select(.focused == true) | .scale
 
 # Scale configuration based on selected layout
 case "${style_index}" in
-1)
-  button_columns=3
-  export x_margin=$((screen_width * 32 / scale_factor))
-  export y_margin=$((screen_height * 28 / scale_factor))
+	1)
+		button_columns=3
+		export x_margin=$((screen_width * 32 / scale_factor))
+		export y_margin=$((screen_height * 28 / scale_factor))
+		export border_rad=$((screen_height / scale_factor /3))
 
-  #export hover_margin=$((screen_height * 33 / scale_factor))
-  ;;
-2)
-  button_columns=2
-  export x_margin=$((screen_width * 38 / scale_factor))
-  export y_margin=$((screen_height * 28 / scale_factor))
+  		#export hover_margin=$((screen_height * 33 / scale_factor))
+  		;;
+	2)
+		button_columns=2
+		export x_margin=$((screen_width * 38 / scale_factor))
+		export y_margin=$((screen_height * 28 / scale_factor))
+		export border_rad=$((screen_height / scale_factor /3))
 
-  # currently not used
-  #export x_hover=$((screen_width * 35 / scale_factor))
-  #export y_hover=$((screen_height * 23 / scale_factor))
-  ;;
+		# currently not used
+		#export x_hover=$((screen_width * 35 / scale_factor))
+		#export y_hover=$((screen_height * 23 / scale_factor))
+		;;
 esac
 
 # Substitute variables in the style template
