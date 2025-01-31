d4f01dc564c45645f4e421f1edb89d455fb067f3
Elia Nitsche <nitscheelia@gmail.com>
2025-01-31 20:24:39 +0100

    scripts: package available test

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -3,8 +3,26 @@
 # Needs: grim slurp wl-clipboard
 # (by egnrse)
 
+# used for notifications
+scriptName="screenshot.sh"
+
 arg1=$1
 
+# test if package ($1) is available (notifies the user if not)
+available() {
+	package=$1
+	if ! pacman -Q "${package}" >>/dev/null 2>&1; then
+		echo "${scriptName}: package '${package}' missing"
+		notify-send "${scriptName}: package '${package}' missing" &
+		return 1
+	fi
+	return 0
+}
+available grim
+available slurp
+
+
+## actually do the screenshot
 if [ -z "${arg1}" ]; then
 	grim -g "$(slurp)" - | wl-copy
 else
--- scripts/updateKDEcache.sh
+++ scripts/updateKDEcache.sh
@@ -6,6 +6,8 @@
 
 if ! pacman -Q archlinux-xdg-menu >/dev/null 2>&1; then
 	echo "'archlinux-xdg-menu' not found this might not work, install it or change the 'XDG_MENU_PREFIX' within this script."
+	notify-send "'archlinux-xdg-menu' not found this might not work"\
+		"install it or change the 'XDG_MENU_PREFIX' within this script." &
 fi
 XDG_MENU_PREFIX=arch-
 kbuildsycoca6 --noincremental
