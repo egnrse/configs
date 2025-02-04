01fb3dd2c38d0f7207e256210d90babed34c39be
Elia Nitsche <nitscheelia@gmail.com>
2025-02-04 17:11:46 +0100

    waybar: hyprWindowMode (make script looping)

---
+++ waybar/scripts/old_hyprWindowMode.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+# looks if the active window (in hyprland) is one of the following:
+# 	normal		: n	(also adds '[]' around the state letter)
+# 	floating	: f	(also adds '()' around the state letter)
+# 	maximized	: M
+# 	fullscreen: F	(also for fullscreen and maximized)
+# 	empty WS	: h (hyprland)
+# 	pseudo		: P
+# 	pinned		: p
+# (or a combination of them)
+# 	eg. (M) means a floating window that is maximized
+# returns the result in a json that works for waybar
+# also toggles the floating state of the active window, when $1="toggle"
+# 
+# Needs:
+# 	hyprland
+# 	awk
+# 	grep
+# 	jq
+#
+#	by egnrse (https://github.com/egnrse/configs)	
+
+# this scripts name (used for notifications)
+scriptName="hyprWindowMode.sh"
+
+# expire time of notifications in ms (see -t in `man notify-send`)
+notification_time=1000
+
+# shows hints if 1
+show_hints=1
+
+
+args1=$1
+
+clientActive=$(hyprctl activewindow -j)
+
+# === toggle ===
+# toggle floating sate of the active window
+if [ "$args1" == "toggle" ]; then
+	out=$(echo "$clientActive" | jq ".address")
+	if [ "$out" = "null" ]; then
+		# no active window found (eg. empty hyperland window)
+		notify-send -u low -a ${scriptName} -t ${notification_time} "${scriptName}: nothing to toggle" &
+		exit 1
+	fi
+	hyprctl dispatch togglefloating
+	varExit=$?
+	if [ $varExit -eq 0 ]; then
+		notify-send -u low -a ${scriptName} -t ${notification_time} "${scriptName}: toggle floating" &
+		#notify-send -u low -a ${scriptName} -r 1683 "toggle floating" &
+	else
+		notify-send -a ${scriptName} "${scriptName}: toggle ERROR?"\
+			"'hyprctl dispatch togglefloating' exited with status '$varExit'" &
+	fi
+	exit 0
+fi
+
+
+# === normal ===
+textOutput="h"
+tooltip="just hyprland"
+if [ $(echo "$clientActive" | jq ".fullscreen") == "0" ]; then
+	textOutput="n"
+	tooltip="normal (f0)"
+fi
+if [ $(echo "$clientActive" | jq ".fullscreen") == "1" ]; then
+	textOutput="M"
+	tooltip="maximized (f1)"
+elif [ $(echo "$clientActive" | jq ".fullscreen") == "2" ]; then
+	textOutput="F"
+	tooltip="fullscreen (f2)"
+elif [ $(echo "$clientActive" | jq ".fullscreen") == "3" ]; then
+	textOutput="F"
+	tooltip="maximized and fullscreen (f3)"
+elif [ $(echo "$clientActive" | jq ".pinned") == "true" ]; then
+	textOutput="p"
+	tooltip="pinned"
+fi
+# pseudo
+pseudo=0
+if [ $(echo "$clientActive" | jq ".pseudo") == "true" ]; then
+	if [ "$textOutput" = "n" ]; then
+		textOutput="P"
+	fi
+	pseudo=1
+fi
+
+# floating
+floating=0
+if [ $(echo "$clientActive" | jq ".floating") == "true" ]; then
+	floating=1
+	case "$textOutput" in
+		# normal
+		"n"|"P")
+			textOutput="f"
+			tooltip="floating"
+			;;
+		# maximized/fullscreen/pinned
+		*)
+			tooltip="${tooltip} and floating"
+			;;
+	esac
+fi
+
+if [ "${pseudo}" -eq 1 ]; then
+	tooltip="${tooltip}\n[pseudo]"
+fi
+if [ $show_hints -eq 1 ]; then
+	if [ "${textOutput}" = "h" ]; then
+		:
+	else
+		tooltip="${tooltip}\n<span font_size='80%'>(toggle floating)</span>"
+	fi
+fi
+
+
+if [ "${floating}" -eq 1 ]; then
+	textOutput="(${textOutput})"
+else
+	textOutput="[${textOutput}]"
+fi
+
+echo "{\"text\":\"${textOutput}\", \"tooltip\":\"${tooltip}\"}"
+
+# for debugging
+#notify-send -u low "{\"text\":\"${textOutput}\", \"tooltip\":\"${tooltip}\"}"
