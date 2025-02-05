0ac74d1677099d6af488f84b4d81ca818887f405
Elia Nitsche <nitscheelia@gmail.com>
2025-02-05 23:22:52 +0100

    DB scripts: made saver

--- other/old/downloadDB.sh
+++ other/old/downloadDB.sh
@@ -1,15 +1,15 @@
 #!/bin/env bash
 
-# sync the DB folder from google drive with the local DB folder ($localPath)
+# copy the DB folder from google drive into the local DB folder ($localPath)
 # by egnrse
 
 localPath=~/Documents/DB/
 
 echo "syncing gdrive-nit:/DB with ${localPath}"
-rclone sync gdrive-nit:/DB ${localPath}
+rclone copy gdrive-nit:/DB ${localPath}
 if [ $? -eq 0 ]; then
 	notify-send "downloadDB successful" "syncing 'gdrive-nit:/DB' with '${localPath}'"
 else
 	notify-send "downloadDB probably failed" "syncing 'gdrive-nit:/DB' with '${localPath}'"
 fi
-#rclone copy gdrive-nit:/DB ~/Documents/DB/
+#rclone sync gdrive-nit:/DB ~/Documents/DB/
