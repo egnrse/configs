4a3d19a84393df9286f2ca713f61d759c7202666
Elia Nitsche <nitscheelia@gmail.com>
2025-02-13 16:49:20 +0100

    uploadDB: fix typo

--- other/old/uploadDB.sh
+++ other/old/uploadDB.sh
@@ -7,7 +7,7 @@
 localPath=~/Documents/DB/
 
 echo "syncing '${localPath}' with 'gdrive-nit:/DB'"
-rclone copy ${localPath} gdrive-nit:/DB --include "KeePassElia2023_06.kbdx"
+rclone copy ${localPath} gdrive-nit:/DB --include "KeePassElia2023_06.kdbx"
 if [ $? -eq 0 ]; then
 	notify-send "uploadDB successful" "syncing '${localPath}' with 'gdrive-nit:/DB'"
 else
